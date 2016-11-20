31d731c91587c609bc6639817c3342aba6855679
Sameer Rahmani <lxsameer@gnu.org>
2016-11-20 15:17:32 +0330

    hellhound's defsystem replaced the system one

--- src/clj/garm/system.clj
+++ src/clj/garm/system.clj
@@ -2,36 +2,14 @@
   (:require [com.stuartsierra.component :as component]
             [hellhound.components.websocket :refer [websocket-server]]
             [hellhound.components.webserver :refer [webserver]]
-            [system.core  :refer [defsystem]]
+            [hellhound.system  :refer [defsystem]]
             [system.repl  :refer [set-init! start]]
-            [garm.handler :refer [routes]]
-            ))
-
-(defn create-system
-  "Create the system map."
-  [routes]
-  (-> (component/system-map)
-      (websocket-server)
-      (webserver routes)))
-
-;; (defn -main [& args]
-;;   (let [host     (first args)
-;;         port-num (first (rest args))
-;;         port     (read-string port-num)]
-
-;;     (if-not (number? port)
-;;       (println "Daah, Port number should be a NUMBER.")
-;;       (do
-;;         (-> (create-system host port)
-;;             (component/start))))))
-
-(defn dev-system
-  []
-  (-> (component/system-map)
-      (websocket-server)
-      (webserver routes)))
+            [garm.handler :refer [routes]]))
 
 
+(defsystem dev-system
+  (websocket-server)
+  (webserver routes))
 
 (defn -main [& args]
   (set-init! #'dev-system)
